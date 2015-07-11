a60a9fd943d5d2cc66fec6886ce680b7633d882d
Quan <quan@Quans-MacBook-Pro.local>
2015-07-11 08:51:57 +0700

    First Commit

---
+++ FunMusic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FunMusic
+//
+//  Created by Quan on 7/11/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FunMusic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunMusic
+//
+//  Created by Quan on 7/11/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
